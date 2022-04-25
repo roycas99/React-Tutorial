7768308d2f165e29cc97f00e9932ae9ba8ddcdda
Abshir J Mohamed <ajmohamed@mplspha.org>
2022-04-25 15:04:46 -0500

    lesson 7-9

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 const Home = () => {
+
+const[name,setName] = useState("Ahmed");
+const handleClick = (ah) => {
+    console.log("Hello " + ah);
+    setName("Ali")
+}
+
+
     return (  
         <div className="home">
             <h2>Home Page </h2>
+            <button onClick={()=>  handleClick("Ahmed")}>Click</button>
+            <p>{ name} </p>
         </div>
     );
 }
