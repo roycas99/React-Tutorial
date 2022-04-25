aa609a7001d8d606e52a1e0290df9a8bc66d007e
Abshir J Mohamed <ajmohamed@mplspha.org>
2022-04-25 13:45:45 -0500

    basics for reacts

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (  
+        <nav className="navbar">
+            <h1> Kirchoff Memories</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New BLOG</a>
+
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
