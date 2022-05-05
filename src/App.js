9484b61706ad9d68648beb782c5c1b6cac1bc518
Abshir J Mohamed <ajmohamed@mplspha.org>
2022-05-05 13:52:36 -0500

    All about Prop and filterArray function delete arr
    see documnet txt from 11 to 13

--- src/App.js
+++ src/App.js
@@ -5,22 +5,16 @@ import { useState } from 'react';
 function App() {
  // variable dynamic value 
  //const title = "Welcome to the Abshir Blog"
-const[blogs,setBlogs] = useState([
-  {title:"nightwalker", Author:"Abshir", id:1},
-  {title: "what", Author:"unknow",id:2}
-])
+
 
   return (
     <div className="App">
       <Navbar/>
       <div className="content">
-       {blogs.map((blog) => (
-         <div className="blog-preview" key={blog.id}>
-           <h2>{blog.title}</h2>
-           <p>written by {blog.Author}</p>
+      <Home/>
          </div>
-       ))}
-      </div>
+       
+      
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,34 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
-const[name,setName] = useState("Ahmed");
-const handleClick = (ah) => {
-    console.log("Hello " + ah);
-    setName("Ali")
-}
+
+ const[blogs,setBlogs] = useState([
+        {title:"nightwalker", Author:"Abshir", id:1},
+        {title: "what", Author:"unknow",id:2}
+      ]);
+
+      const handleDelete = (id) => {
+          const newBlogs = blogs.filter((blog) => blog.id !== id)
+          setBlogs(newBlogs);
+
+      }
+
+// const[name,setName] = useState("Ahmed");
+// const handleClick = (ah) => {
+//     console.log("Hello " + ah);
+//     setName("Ali")
+// }
 
 
     return (  
         <div className="home">
-            <h2>Home Page </h2>
-            <button onClick={()=>  handleClick("Ahmed")}>Click</button>
-            <p>{ name} </p>
+            <BlogList  blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
+            <BlogList  blogs={blogs.filter((blogs) => blogs.Author === "Abshir")} title="Abshir Blog"/>
         </div>
-    );
-}
+            );
+
+            }
  
 export default Home;
