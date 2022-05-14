a560a3d3180d65079080e811772fa3ad50789c88
roycas99 <jamesblack1319@gmail.com>
2022-05-14 16:29:45 -0500

    useEffect Clearup and AbortController #24

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,38 +1,47 @@
 // # 20
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  // #18
+  const [isPending, setIsPending] = useState(true);
+  //#19
+  const [error, setEror] = useState(null);
 
+  useEffect(() => {
+    //24
+    const abortCont = new AbortController();
 
-const useFetch  = (url) => {
+    setTimeout(() => {
+      fetch(url, { signal: abortCont.signal })
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("we could not fetch data ");
+          }
+          console.log(res);
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setEror(null);
+        })
+        .catch((err) => {
+          // when fetch throw error. if err is abort,display fetch aborted#24
+          if (err.name === "AbortError") {
+            console.log("fetch aborted");
+          } else {
+            setEror(err.message);
+            setIsPending(false);
+          }
+        });
+    }, 1000);
 
-    const[data,setData] = useState(null)
-    // #18
-      const [isPending,setIsPending] = useState(true);
-      //#19
-      const [error,setEror] = useState(null);
+    // #24
+    return () => abortCont.abort();
+  }, [url]);
 
-      useEffect(() => {
-        setTimeout(() => {
-          fetch(url).then((res)=>{
-              if(!res.ok){
-                  throw Error("we could not fetch data ")
-              }
-              console.log(res)
-              return res.json();
-          }).then((data)=>{
-              setData(data);
-              setIsPending(false);
-              setEror(null)
-          }).catch((err)=> {
-              setEror(err.message)
-              setIsPending(false);
-          })
+  return { data, isPending, error };
+};
 
-        },1000);
-        
-    },[url]);
-
-    return { data, isPending,error };
-}
- 
 export default useFetch;
